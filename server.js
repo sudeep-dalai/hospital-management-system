d540be5c944a7191e0633d336a0ad1dfdf015ba3
Sudeep dalai <sudeep2530@gmail.com>
2026-04-15 11:45:02 +0530

    Removed sqlite पूरी तरह

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
-const sqlite3 = require("sqlite3").verbose();
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,13 +19,13 @@ app.use(
   })
 );
 
-const db = new sqlite3.Database("hospital.db", (err) => {
+
   if (err) {
     console.error("Database connection error:", err.message);
   } else {
     console.log("Connected to SQLite database.");
   }
-});
+;
 
 db.serialize(() => {
   db.run(`
